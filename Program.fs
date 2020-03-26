272b7a44bb92ec884af6cbc1d0839d9024ed107c
Régis Melgaço de Andrade <regis.melgaco@gmail.com>
2020-03-26 09:33:30 -0300

    :D

---
+++ Program.fs
@@ -0,0 +1,28 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let fib n =
+    let rec recFib(n, mem: Map<int, double>) =
+        if mem.ContainsKey n then
+            (mem.[n], mem)
+        elif n <= 1 then
+            let newMem = Map.ofList [0, 0.0; 1, 1.0]
+            (double n, newMem)
+        else
+            let (fibMinus2, mem2) = recFib(n-2, mem)
+            let (fibMinus1, mem1) = recFib(n-1, mem2)
+            let value = fibMinus1 + fibMinus2
+            (value, mem1.Add(n, value))
+
+    let (value, _) = recFib(n, Map.empty)
+    value
+
+
+[<EntryPoint>]
+let main argv =
+    let element = argv.[0] |> int
+    let value = fib element
+
+    printfn "The %d of fibonacci is %.0f" element value
+    0 // return an integer exit code
